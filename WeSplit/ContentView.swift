3fc99fb01b07c7f0e52662e83d789f0e15d080c6
pranay-vohra <pranayvohra77@gmail.com>
2024-11-03 00:28:11 +0530

    Initial Commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by pranay vohra on 02/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var buttonCount:Int=0
+    var body: some View {
+       
+        Form{
+            Section("tf"){
+                Button("button Count: \(buttonCount)"){
+                    buttonCount+=1
+                }
+                
+            }
+            Section{
+                Button("button Count: \(buttonCount)"){
+                    buttonCount+=1
+                }
+            }
+        }
+
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by pranay vohra on 02/11/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
